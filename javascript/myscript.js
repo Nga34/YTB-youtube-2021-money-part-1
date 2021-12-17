d01580c670a632e6993a77e33cfda69fe663125a
Nga34 <96304228+Nga34@users.noreply.github.com>
2021-12-17 23:40:07 +0700

    Update myscript.js

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -190,9 +190,9 @@ getclickf.addEventListener('click', function(){
 var cantho =document.querySelector('.main');
 cantho.insertAdjacentHTML('afterend', '<div class="author mt-5"><div class="row"><div class="col-12"><div class="me text-danger text-right"></div></div></div></div>');
 
-// Phần popup khi bạn nhấn vào nút "Nhịp tim của anh"
+// Phần popup khi bạn nhấn vào nút "Nhịp tim của mình là "
 document.querySelector('.Description .modal-title').innerHTML = '<i class="far fa-comment-alt"></i>&nbsp;Nhịp tim như vậy là tại vì:';
-document.querySelector('.Description .modal-body').innerHTML = 'Anh thích em đó <i class="fas fa-heart"></i> ';
+document.querySelector('.Description .modal-body').innerHTML = 'CHO MÌNH XIN 50K NHÉ :))) <i class="fas fa-heart"></i> ';
 var author = document.querySelector('.author .me');author.style.opacity = "0.5";
 
 author.innerHTML = "Mọi thắc mắc bạn có thể liên hệ<a href='https://www.facebook.com/PhungQuocDinh/'>Quốc Định<a>";
