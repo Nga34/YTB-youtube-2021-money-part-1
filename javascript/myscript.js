ce81b0bc2e05f129f93a4eb1c8dbf547f4c6c8fc
Nga34 <96304228+Nga34@users.noreply.github.com>
2021-12-18 00:15:46 +0700

    Update myscript.js

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -190,7 +190,7 @@ getclickf.addEventListener('click', function(){
 var cantho =document.querySelector('.main');
 cantho.insertAdjacentHTML('afterend', '<div class="author mt-5"><div class="row"><div class="col-12"><div class="me text-danger text-right"></div></div></div></div>');
 
-// Phần popup khi bạn nhấn vào nút "Nhịp tim của mình là "
+// Phần popup khi bạn nhấn vào nút "Nhịp tim của mình hiện là"
 document.querySelector('.Description .modal-title').innerHTML = '<i class="far fa-comment-alt"></i>&nbsp;Nhịp tim như vậy là tại vì:';
 document.querySelector('.Description .modal-body').innerHTML = 'CHO MÌNH XIN 50K NHÉ :))) <i class="fas fa-heart"></i> ';
 var author = document.querySelector('.author .me');author.style.opacity = "0.5";
